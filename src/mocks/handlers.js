12ab6ebf778a297eef61c82007b65e3eb971f1b4
Shahmir Khan Jadoon <shahmirkj@gmail.com>
2022-06-14 15:33:40 +0500

    Completed the Order Phase

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -31,6 +31,9 @@ export const handlers = [
 						{ name: 'Hot fudge', imagePath: '/images/hot-fudge.png' },
 					])
 				);
+			}),
+			rest.post('http://localhost:3030/order', (req, res, ctx) => {
+				return res(ctx.json({ orderNumber: 123455676 }));
 			})
 		);
 	}),
